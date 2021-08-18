326eb83264629cd65e0120307b142670f98540ce
jeonbyeongmin <qudals7613@gmail.com>
2021-08-18 23:42:55 +0900

    add router/regist

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
-const config = require("./config/key");
 const mongoose = require("mongoose");
 
+// Router
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const registRouter = require("./routers/regist");
+
+// Key for hiding DB password
+const config = require("./config/key");
+
+// Create app
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -31,15 +31,18 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
+// Middleware
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/regist", registRouter);
+
+// connect mongoDB with mongoose
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
